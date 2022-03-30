a5a5a478f95e5f0795c0233f7f7ff913c93896d1
Konstantin Ladutenko <kostyfisik@gmail.com>
2022-03-30 14:05:13 +0300

    add QCalendar

---
+++ src/pages/test.vue
@@ -0,0 +1,48 @@
+<!-- https://github.com/quasarframework/quasar-ui-qcalendar/blob/next/docs/src/examples/ResourceChildren.vue -->
+<script setup lang="ts">
+import { QCalendarResource, today } from '@quasar/quasar-ui-qcalendar/src/QCalendarResource.js'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarVariables.sass'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarTransitions.sass'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarResource.sass'
+
+const selectedDate = ref(today())
+const resources = ref([
+  { id: '1', name: 'John' },
+  {
+    id: '2',
+    name: 'Board Room',
+    expanded: false,
+    children: [
+      { id: '2.1', name: 'Room-1' },
+      {
+        id: '2.2',
+        name: 'Room-2',
+        expanded: false,
+        children: [
+          { id: '2.2.1', name: 'Partition-A' },
+          { id: '2.2.2', name: 'Partition-B' },
+          { id: '2.2.3', name: 'Partition-C' },
+        ],
+      },
+    ],
+  },
+  { id: '3', name: 'Mary' },
+  { id: '4', name: 'Susan' },
+  { id: '5', name: 'Olivia' },
+])
+
+</script>
+<template>
+  <div class="row justify-center">
+    <div style="display: flex; max-width: 800px; width: 100%; height: 400px;">
+      <q-calendar-resource
+        ref="calendar"
+        v-model="selectedDate"
+        v-model:model-resources="resources"
+        resource-key="id"
+        resource-label="name"
+        bordered
+      />
+    </div>
+  </div>
+</template>

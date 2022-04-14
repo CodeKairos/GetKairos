ac495a8c9333c21e936d9bb387189187f767f18f
Konstantin Ladutenko <kostyfisik@gmail.com>
2022-04-14 16:43:33 +0300

    update cypress test

--- cypress/integration/remote.spec.ts
+++ cypress/integration/remote.spec.ts
@@ -13,22 +13,22 @@ context('Remote API', () => {
   //   expect(add(1, 2)).to.eq(3)
   // })
 
-  it('can add and remove a bookable type', () => {
-    const initial = api.getAllBookableTypes().length
+  it('can add and remove a bookable type', async() => {
+    const initial = (await api.getAllBookableTypes()).length
     const additionaType = 'some type'
 
     api.addBookableType(additionaType)
     let data = api.getAllBookableTypes()
-    expect(data.includes(additionaType)).to.eq(true)
+    expect((await data).includes(additionaType)).to.eq(true)
 
-    const incremented = data.length
+    const incremented = (await data).length
     expect(initial).to.eq(incremented - 1)
 
     api.deleteBookableType(additionaType)
     data = api.getAllBookableTypes()
-    expect(data.includes(additionaType)).to.eq(false)
+    expect((await data).includes(additionaType)).to.eq(false)
 
-    const final = data.length
+    const final = (await data).length
     expect(initial).to.eq(final)
   })
 })

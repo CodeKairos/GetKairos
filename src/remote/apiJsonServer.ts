03b56fd3e771acd9a85bbdbce71f159b6b9c9912
Konstantin Ladutenko <kostyfisik@gmail.com>
2022-04-19 22:15:16 +0300

    extract generic API methods to deal with tag clouds

--- src/remote/apiJsonServer.ts
+++ src/remote/apiJsonServer.ts
@@ -1,6 +1,39 @@
+import type { AxiosInstance } from 'axios'
 import axios from 'axios'
 import type { ApiContract } from '~/remote/apiContract'
 const bookableTypes = '/bookableTypes'
+
+async function getAllTags(http: AxiosInstance, endpoint: string) {
+  let responseData: { id: number; name: string }[] = []
+  try {
+    const response = await http.get(endpoint)
+    responseData = response.data
+  }
+  catch (err) {
+  }
+  return responseData.map((e: { id: number; name: string }) => e.name)
+}
+
+async function addTag(http: AxiosInstance, endpoint: string, tagName: string) {
+  await http.post(endpoint, { name: tagName })
+}
+
+async function deleteTag(http: AxiosInstance, endpoint: string, tagName: string) {
+  let responseData: { id: number; name: string }[] = []
+
+  try {
+    const response = await http.get(endpoint)
+    responseData = response.data
+  }
+  catch (err) {
+  }
+  for (const record of responseData) {
+    if (record.name === tagName) {
+      // console.log(record, 'to delete:', name, ' request:', `${bookableTypes}/${record.id}`)
+      await http.delete(`${endpoint}\\${record.id}`)
+    }
+  }
+}
 
 class ApiJsonServer implements ApiContract {
   private baseURL = ''
@@ -16,54 +49,15 @@ class ApiJsonServer implements ApiContract {
   }
 
   async getAllBookableTypes() {
-    let responseOld: { id: number; type: string }[] = []
-    try {
-      const response = await this.http.get(bookableTypes)
-      responseOld = response.data
-    }
-    catch (err) {
-    }
-    // console.log('getAll:',
-    //   responseOld.map((e: { id: number; type: string }) => e.type),
-    // )
-    return responseOld.map((e: { id: number; type: string }) => e.type)
+    return await getAllTags(this.http, bookableTypes)
   }
 
-  async addBookableType(name: string): Promise<void> {
-    const postData = {
-      type: name,
-    }
-    // try {
-    //   // const response =
-    //   await this.http.get(bookableTypes)
-    //   // responseOld = response.data
-    // }
-    // catch (err) {
-    // }
-    // const res =
-    await this.http.post(bookableTypes, postData)
-    // const result = {
-    //   status: `${res.status}-${res.statusText}`,
-    //   headers: res.headers,
-    //   data: res.data,
-    // }
-    // console.log(result)
+  async addBookableType(typeName: string): Promise<void> {
+    await addTag(this.http, bookableTypes, typeName)
   }
 
-  async deleteBookableType(name: string): Promise<void> {
-    let responseOld: { id: number; type: string }[] = []
-    try {
-      const response = await this.http.get(bookableTypes)
-      responseOld = response.data
-    }
-    catch (err) {
-    }
-    for (const record of responseOld) {
-      if (record.type === name) {
-        // console.log(record, 'to delete:', name, ' request:', `${bookableTypes}/${record.id}`)
-        await this.http.delete(`${bookableTypes}\\${record.id}`)
-      }
-    }
+  async deleteBookableType(typeName: string): Promise<void> {
+    await deleteTag(this.http, bookableTypes, typeName)
   }
 }
 export { ApiJsonServer }

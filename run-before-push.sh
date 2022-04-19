d984f796975ab6cbcab7c135c2f762bdda289ce9
Konstantin Ladutenko <kostyfisik@gmail.com>
2022-04-19 10:43:51 +0300

    update

--- run-before-push.sh
+++ run-before-push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+pnpm run jsonServer &
+npx vite --port 3333 &
 pnpm run lint
 pnpm run typecheck
 npx vitest --run
-# npx cypress run --spec 'cypress/integration/*.spec.ts' 
+# pnpm run build
+npx cypress run --spec 'cypress/integration/*.spec.ts' 

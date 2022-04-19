df047f02fa65105fb8ef52f51042a36225a049c8
Vladimir <v.igoshin01@mail.ru>
2022-04-20 01:28:10 +0300

    possible implementation of basic data types

--- src/remote/apiContract.ts
+++ src/remote/apiContract.ts
@@ -1,3 +1,37 @@
+interface  BookableItem {
+  id: number
+  name: string
+  type: string
+  tags?: string[]
+  photos?: string[]
+  icon?: string
+  relatedProps?: {[key: string]: any}[]
+}
+
+interface  User {
+  id: number
+  name: string
+  image?: string
+  sm_image?: string
+  tags?: string[]
+}
+
+interface  relatedUser extends User {
+  role: string
+}
+
+interface  BookingEvent {
+  id: number
+  bookableItemId: number
+  start: string
+  end: string
+  creator: User
+  relatedUsers: relatedUser[]
+  tags?: string[]
+}
+
+export {BookableItem, User, BookingEvent}
+
 export interface ApiContract {
   addBookableType(name: string): Promise<void>
   getAllBookableTypes(): Promise<string[]>
